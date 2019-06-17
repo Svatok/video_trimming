1dcf69b75c28710a1128fbcb68c452c3eab70408
Svatok <s_s_s@ua.fm>
2019-06-17 18:18:20 +0300

    Update readme (#10)
    * Update readme
    
    * Update apipie examples

--- app/docs/api/v1/videos_doc.rb
+++ app/docs/api/v1/videos_doc.rb
@@ -5,13 +5,13 @@ module Api
 
       api :GET, '/v1/videos', 'Show all videos'
 
-      header 'Authorization', 'Bearer authorization_id', required: true
+      header 'Authorization', 'Bearer identificator', required: true
 
       def index; end
 
       api :POST, '/v1/videos', 'Create trimmed video'
 
-      header 'Authorization', 'Bearer authorization_id', required: true
+      header 'Authorization', 'Bearer identificator', required: true
 
       param :name, String, required: true
       param :source_video, File, required: true
--- spec/requests/requests/restarting_spec.rb
+++ spec/requests/requests/restarting_spec.rb
@@ -1,4 +1,4 @@
-RSpec.describe 'Restarting', type: :request do
+RSpec.describe 'Restarting', :show_in_doc, type: :request do
   let(:user) { create :user }
   let(:video) { create :video, :with_source_video, user: user }
   let(:authorization_header) { authorization_header_for(user) }
@@ -10,7 +10,7 @@ RSpec.describe 'Restarting', type: :request do
           post api_v1_request_restarting_path(request_id: 'request_id')
         end
 
-        it 'renders errors', :show_in_doc do
+        it 'renders errors' do
           expect(response).to be_unauthorized
         end
       end
@@ -22,7 +22,7 @@ RSpec.describe 'Restarting', type: :request do
           post api_v1_request_restarting_path(request_id: request_id), headers: authorization_header
         end
 
-        it 'renders errors', :show_in_doc do
+        it 'renders errors' do
           expect(response).to be_not_found
         end
       end
