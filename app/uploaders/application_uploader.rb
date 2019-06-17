f1326099be6279dc283665243e2b7ef939fd7293
Svatok <s_s_s@ua.fm>
2019-06-17 12:54:28 +0300

    Add sidekiq web

--- app/uploaders/application_uploader.rb
+++ app/uploaders/application_uploader.rb
@@ -4,4 +4,5 @@ class ApplicationUploader < Shrine
   plugin :determine_mime_type
   plugin :add_metadata
   plugin :processing
+  plugin :default_url_options, store: { host: 'http://localhost:3000' }
 end
